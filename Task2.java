ca9eb10bd1cc78793df1a759916de0e4ab140701
adeahmed <98258192+adeahmed@users.noreply.github.com>
2023-08-22 15:41:56 +0300

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args) {
+        int rivi = 5;
+
+        for (int i = 1; i <= rivi; i++) {
+            System.out.println(" ".repeat(rivi - i) + "*".repeat(2 * i - 1));
+        }
+    }
+}
