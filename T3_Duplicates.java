9d3548356ee58f0e54195f200b044d338fce085d
adeahmed <98258192+adeahmed@users.noreply.github.com>
2023-08-26 20:50:59 +0300

    Updatea

---
+++ T3_Duplicates.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class T3_Duplicates {
+    public static void main(String[] args) {
+        ArrayList<Integer> taulukko = new ArrayList<>(); // Muista että muuttuja nimi lisätään ennen = merkkiä!!!
+        ArrayList<Integer> tulosTaulukko = new ArrayList<>();
+        Scanner lukija = new Scanner(System.in);
+        System.out.print("Syötä taulukon koko: ");
+        int luku = lukija.nextInt();
+        for (int i = 0; i < luku; i++) {
+            System.out.print("Anna kokonaisluku " + (i + 1) + ": ");
+            taulukko.add(lukija.nextInt());
+        }
+
+        for (int lukuTulos : taulukko) {
+            if (!tulosTaulukko.contains(lukuTulos)) {
+                tulosTaulukko.add(lukuTulos);
+            }
+        }
+        System.out.println("Taulukko ilman kaksoiskappaleita:");
+        for (int lukuTulos : tulosTaulukko) {
+            System.out.print(lukuTulos + " ");
+        }
+        lukija.close();
+    }
+}
---
+++ T3_Maximum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class T3_Maximum {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        ArrayList<Integer> taulukko = new ArrayList<>();
+        int maxSum = Integer.MIN_VALUE;
+        int starttiIndex = 0;
+        int endIndex = 0;
+
+        System.out.print("Syötä taulukon kokonaislukujen lukumäärä: ");
+        int n = lukija.nextInt();
+
+        System.out.println("Syötä kokonaisluvut taulukkoon:");
+
+        for (int i = 0; i < n; i++) {
+            taulukko.add(lukija.nextInt());// -> Suoraan taulu...
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += taulukko.get(j);
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    starttiIndex = i;
+                    endIndex = j;
+                }
+            }
+        }
+        System.out.println(taulukko); // alkiot
+        System.out.println("Suurin summa: " + maxSum);
+        System.out.println("Alitaulukon indeksit: " + (starttiIndex + 1) + " - " + (endIndex + 1));
+
+        lukija.close();
+    }
+}
